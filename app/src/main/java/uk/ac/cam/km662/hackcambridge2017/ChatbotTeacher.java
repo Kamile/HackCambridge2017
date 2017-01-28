4ce89a0a632aee6d0f1198c7394ec54eb4bb2abb
kamile <km662@cam.ac.uk>
2017-01-28 22:23:10 +0000

    Added adapter for ListView

--- app/src/main/java/uk/ac/cam/km662/hackcambridge2017/ChatbotTeacher.java
+++ app/src/main/java/uk/ac/cam/km662/hackcambridge2017/ChatbotTeacher.java
@@ -1,15 +1,20 @@
 package uk.ac.cam.km662.hackcambridge2017;
 
+import android.app.ListActivity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import static android.R.id.list;
 import static android.R.id.message;
 import static java.security.AccessController.getContext;
 
@@ -18,9 +23,13 @@ public class ChatbotTeacher extends AppCompatActivity {
     private EditText messageBodyField;
     private String messageBody;
     private ListView messagesList;
+    private MessageAdapter messageAdapter;
 
     private View userMessage;
     TextView txtMessage;
+
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listItems = new ArrayList<String>();
 
 
 
@@ -31,7 +40,14 @@ public class ChatbotTeacher extends AppCompatActivity {
 
         messageBodyField = (EditText) findViewById(R.id.messageBodyField);
         messagesList = (ListView) findViewById(R.id.listMessages);
-        txtMessage = (TextView) userMessage.findViewById(R.id.txtMessage);
+
+        messageAdapter = new MessageAdapter(this);
+        messagesList.setAdapter(messageAdapter);
+
+
+
+        //adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        //messagesList.setAdapter(adapter);
 
 
         //listen for a click on the send button
@@ -50,12 +66,14 @@ public class ChatbotTeacher extends AppCompatActivity {
             return;
         }
 
-        LayoutInflater inflater = this.getLayoutInflater();
 
-        userMessage = getLayoutInflater().inflate(R.layout.user_message,  null);
-        txtMessage.setText(messageBody);
-        messagesList.addView(userMessage);
+
+        messageAdapter.addMessage(messageBody, MessageAdapter.DIRECTION_USER);
+//
+
+
+        //need to notify datasetchanged
         messageBodyField.setText("");
-
+        messageBodyField.setHint("Type a message . . . ");
     }
 }
---
+++ app/src/main/java/uk/ac/cam/km662/hackcambridge2017/MessageAdapter.java
@@ -0,0 +1,78 @@
+package uk.ac.cam.km662.hackcambridge2017;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kamile on 28/01/2017.
+ */
+
+public class MessageAdapter extends BaseAdapter {
+
+
+    public static final int DIRECTION_BOT = 0;
+    public static final int DIRECTION_USER = 1;
+
+    private LayoutInflater layoutInflater;
+    private ArrayList<String> messages;
+
+    public MessageAdapter(Activity activity) {
+        layoutInflater = activity.getLayoutInflater();
+        messages = new ArrayList<String>();
+    }
+
+    public void addMessage(String message, int direction) {
+        messages.add(message);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return messages.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return messages.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+    
+
+    //Adapter to create message bubbles to add to listview
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        int direction = getItemViewType(i);
+
+        //show message on left or right, depending on if
+        //it's incoming or outgoing
+        if (convertView == null) {
+            int res = 0;
+            if (direction == DIRECTION_BOT) {
+                res = R.layout.bot_message;
+            } else if (direction == DIRECTION_USER) {
+                res = R.layout.user_message;
+            }
+            convertView = layoutInflater.inflate(res, viewGroup, false);
+        }
+
+        String message = messages.get(i);
+
+        TextView txtMessage = (TextView) convertView.findViewById(R.id.txtMessage);
+        txtMessage.setText(message);
+
+        return convertView;
+    }
+
+
+}

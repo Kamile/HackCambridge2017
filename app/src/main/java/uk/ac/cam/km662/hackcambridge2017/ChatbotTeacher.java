5594132034955eae0aa3e1dbb04676bb1e3929ff
jwonged <dsjw2@cam.ac.uk>
2017-01-28 23:16:56 +0000

    Update ChatbotTeacher.java

--- app/src/main/java/uk/ac/cam/km662/hackcambridge2017/ChatbotTeacher.java
+++ app/src/main/java/uk/ac/cam/km662/hackcambridge2017/ChatbotTeacher.java
@@ -18,7 +18,25 @@ import static android.R.id.list;
 import static android.R.id.message;
 import static java.security.AccessController.getContext;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ChatbotTeacher extends AppCompatActivity {
+  
+    private Button sendBtn;
+    private String localToken = "";
+    private String conversationId = "";
+    private String primaryToken = "";
+    private String botName = "";
+    //keep the last Response MsgId, to check if the last response is already printed or not
+    private String lastResponseMsgId = "";
+    
+    private GoogleApiClient client;
 
     private EditText messageBodyField;
     private String messageBody;
@@ -43,18 +61,27 @@ public class ChatbotTeacher extends AppCompatActivity {
 
         messageAdapter = new MessageAdapter(this);
         messagesList.setAdapter(messageAdapter);
+        
+        
+        initControls();
 
+        primaryToken = getMetaData(getBaseContext(),"botPrimaryToken");
+        botName = getMetaData(getBaseContext(),"botName").toLowerCase();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        
 
         //adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
         //messagesList.setAdapter(adapter);
-
 
         //listen for a click on the send button
         findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendMessage();
+                pollBotResponses();
             }
         });
     }
@@ -69,5 +96,93 @@ public class ChatbotTeacher extends AppCompatActivity {
         messageAdapter.addMessage(userMessage);
         messageBodyField.setText("");
         messageBodyField.setHint("Type a message . . . ");
+        
+        
     }
+    
+    public void pollBotResponses()
+    {
+        String botResponse = "";
+        if(conversationId != "" && localToken != "") {
+            botResponse = getBotResponse();
+            if (botResponse != "") {
+                try {
+                    JSONObject jsonObject = new JSONObject(botResponse);
+                    String responseMsg = "";
+                    Integer arrayLength = jsonObject.getJSONArray("activities").length();
+                    String msgFrom = jsonObject.getJSONArray("activities").getJSONObject(arrayLength-1).getJSONObject("from").get("id").toString();
+                    String curMsgId = jsonObject.getJSONArray("activities").getJSONObject(arrayLength-1).get("id").toString();
+
+                    if(msgFrom.trim().toLowerCase().equals(botName)) {
+                        if(lastResponseMsgId == "") {
+                            responseMsg = jsonObject.getJSONArray("activities").getJSONObject(arrayLength - 1).get("text").toString();
+                            AddResponseToChat(responseMsg);
+                            lastResponseMsgId = curMsgId;
+                        }
+                        else if(!lastResponseMsgId.equals(curMsgId))
+                        {
+                            responseMsg = jsonObject.getJSONArray("activities").getJSONObject(arrayLength - 1).get("text").toString();
+                            AddResponseToChat(responseMsg);
+                            lastResponseMsgId = curMsgId;
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    /*
+    Add the bot response to chat window
+     */
+    private void AddResponseToChat(String botResponse)
+    {
+        Message message = new Message();
+        //message.setId(2);
+        message.direction(0);
+        message.setMessage(botResponse);
+        displayMessage(message);
+    }
+    
+    public void displayMessage(ChatMessage message) {
+        messageAdapter.addMessage(message);
+        messageAdapter.notifyDataSetChanged();
+        messagesList.setSelection(messagesList.getCount() - 1);
+    }
+    
+    /*
+    Get metadata from manifest file against a given key
+     */
+    public static String getMetaData(Context context, String name) {
+        try {
+            ApplicationInfo ai = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            return bundle.getString(name);
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.w("Metadata", "Unable to load meta-data: " + e.getMessage());
+        }
+        return null;
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+    
 }

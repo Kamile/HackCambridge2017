32621ec5cd6396447b80a6691e4b450812e16c14
jwonged <dsjw2@cam.ac.uk>
2017-01-29 03:43:06 +0000

    Update Score.java

--- app/src/main/java/uk/ac/cam/km662/hackcambridge2017/Score.java
+++ app/src/main/java/uk/ac/cam/km662/hackcambridge2017/Score.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.km662.hackcambridge2017;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by kamile on 28/01/2017.
@@ -12,28 +13,67 @@ public class Score {
     private int correctQuestions = 0;
     private int incorrectQuestions = 0;
     private ArrayList weeklyProgress;
-    private int[] topics = new int[5];
+    private int topicRange[] = new int[5];
+    private int topicScores[] = new int[5];
+    int min, max;
     
     //topic 1-2, 3-5
-    public void setUp() {
-        topics = new int[5];
+    public Score() {
+        for (int i=0; i<topics.length; i++) {
+            topicRange[i] = i*3;
+            topicScores[i] = 0;
+        }
+        min=0;
+        max = 15;
     }
     
     public int getTopic() {
-        
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int sum=0, i=0;
+        //recheck this
+        while (sum < randomNum) {
+            sum += topicsRange[i];
+            i++;
+        }
+        return i;
     }
+    
+    public int[] getTopicScores() { return topicScores;}
     
     public void incrementTopic(int topic, int level) {
         //got correct
+        int i=topic;
+        while (i<topicRange.length) {
+            if (topicScores[i]-level < 0) {
+                topicScores[i] = 0;
+                topicRange[i] = 0;
+            }
+        incrementQuestionScore();
     }
     
-    public void incrementTopic(int topic, int level) {
+    public void decrementTopic(int topic, int level) {
         //got wrong
+        int i=topic;
+        while (i<topicRange.length) {
+            if (topicScores[i]-level < 0) {
+                topicScores[i] = 0;
+                topicRange[i] = 0;
+            }
+        decrementQuestionScore();
     }
     
     //difficulty 1-3
     public int getDifficulty() {
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+        return 0;
+    }
         
+    private void adjustRest(int pos, int amt) {
+        while (pos < topicRange.length) {
+            topicRange[pos] + amt;
+            topicScores[pos] + amt;
+        }
+        max += amt;
     }
 
     public int getCorrectQuestions(){
